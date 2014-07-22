6e211279fcd9d0df6b2b7e65d8c8275456972d03
vnot <f3ri_feb@yahoo.co.id>
2014-07-23 00:00:11 +0700

    Delete People Tracker 1
    Delete People Tracker

--- PeopleTracker/gen/com/threepreneur/f2014/peopletracker/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.threepreneur.f2014.peopletracker;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
